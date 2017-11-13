b9d7c47ea8b18e6a00562cf264d0fca2eef32022
totovr <a.vegaramirez76@gmail.com>
2017-11-13 12:15:28 +0900

    Update

--- Processing 3.3.6/Kinect/User/README.md
+++ Processing 3.3.6/Kinect/User/README.md
@@ -1,3 +1,3 @@
 # Skeleton tracking and angle detection using SimpleOpenni library
 
-  In this program I map 4 angles using Skeleton tracking
+  
--- Processing 3.3.6/Kinect/User/build/source/User.java
+++ Processing 3.3.6/Kinect/User/build/source/User.java
@@ -1,50 +1,68 @@
-import SimpleOpenNI.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import SimpleOpenNI.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class User extends PApplet {
+
+
 
 SimpleOpenNI  context;
-color[]       userClr = new color[]{ color(255,0,0),
+int[]       userClr = new int[]{ color(255,0,0),
                                      color(0,255,0),
                                      color(0,0,255),
                                      color(255,255,0),
                                      color(255,0,255),
                                      color(0,255,255)
                                    };
-PVector com = new PVector();                                   
-PVector com2d = new PVector();                                   
+PVector com = new PVector();
+PVector com2d = new PVector();
 
-void setup()
+public void setup()
 {
-  size(640,480);
   
+
   context = new SimpleOpenNI(this);
   if(context.isInit() == false)
   {
-     println("Can't init SimpleOpenNI, maybe the camera is not connected!"); 
+     println("Can't init SimpleOpenNI, maybe the camera is not connected!");
      exit();
-     return;  
+     return;
   }
-  
-  // enable depthMap generation 
+
+  // enable depthMap generation
   context.enableDepth();
-   
+
   // enable skeleton generation for all joints
   context.enableUser();
- 
+
   background(200,0,0);
 
   stroke(0,0,255);
   strokeWeight(3);
-  smooth();  
+  
 }
 
-void draw()
+public void draw()
 {
   // update the cam
   context.update();
-  
+
   // draw depthImageMap
   //image(context.depthImage(),0,0);
   image(context.userImage(),0,0);
-  
+
   // draw the skeleton if it's available
   int[] userList = context.getUsers();
   for(int i=0;i<userList.length;i++)
@@ -53,8 +71,8 @@ void draw()
     {
       stroke(userClr[ (userList[i] - 1) % userClr.length ] );
       drawSkeleton(userList[i]);
-    }      
-      
+    }
+
     // draw the center of mass
     if(context.getCoM(userList[i],com))
     {
@@ -68,15 +86,15 @@ void draw()
         vertex(com2d.x - 5,com2d.y);
         vertex(com2d.x + 5,com2d.y);
       endShape();
-      
+
       fill(0,255,100);
       text(Integer.toString(userList[i]),com2d.x,com2d.y);
     }
-  }    
+  }
 }
 
 // draw the skeleton with the selected joints
-void drawSkeleton(int userId)
+public void drawSkeleton(int userId)
 {
   // to get the 3d joint data
   /*
@@ -84,7 +102,7 @@ void drawSkeleton(int userId)
   context.getJointPositionSkeleton(userId,SimpleOpenNI.SKEL_NECK,jointPos);
   println(jointPos);
   */
-  
+
   context.drawLimb(userId, SimpleOpenNI.SKEL_HEAD, SimpleOpenNI.SKEL_NECK);
 
   context.drawLimb(userId, SimpleOpenNI.SKEL_NECK, SimpleOpenNI.SKEL_LEFT_SHOULDER);
@@ -104,38 +122,36 @@ void drawSkeleton(int userId)
 
   context.drawLimb(userId, SimpleOpenNI.SKEL_TORSO, SimpleOpenNI.SKEL_RIGHT_HIP);
   context.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_HIP, SimpleOpenNI.SKEL_RIGHT_KNEE);
-  context.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_KNEE, SimpleOpenNI.SKEL_RIGHT_FOOT);  
+  context.drawLimb(userId, SimpleOpenNI.SKEL_RIGHT_KNEE, SimpleOpenNI.SKEL_RIGHT_FOOT);
 }
 
 // -----------------------------------------------------------------
 // SimpleOpenNI events
 
-void onNewUser(SimpleOpenNI curContext, int userId)
+public void onNewUser(SimpleOpenNI curContext, int userId)
 {
   println("onNewUser - userId: " + userId);
   println("\tstart tracking skeleton");
-  
+
   curContext.startTrackingSkeleton(userId);
 }
 
-void onLostUser(SimpleOpenNI curContext, int userId)
+public void onLostUser(SimpleOpenNI curContext, int userId)
 {
   println("onLostUser - userId: " + userId);
 }
 
-void onVisibleUser(SimpleOpenNI curContext, int userId)
+public void onVisibleUser(SimpleOpenNI curContext, int userId)
 {
   //println("onVisibleUser - userId: " + userId);
 }
-
-
-void keyPressed()
-{
-  switch(key)
-  {
-  case ' ':
-    context.setMirror(!context.mirror());
-    break;
+  public void settings() {  size(640,480);  smooth(); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "User" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
-}  
-
+}
