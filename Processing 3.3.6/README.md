99d51f06edcac15cedcf42bba540070d7e71687e
totovr <a.vegaramirez76@gmail.com>
2017-11-15 14:39:53 +0900

    Update

--- Processing 3.3.6/README.md
+++
@@ -1,6 +0,0 @@
-# Processing
-
-Programs using Processing V3.3.6 and Kinect V1, the instructions to Setup the Kinect are written for
-Mac OS user.
-
-# This examples just work with Kinect V1.
---
+++ Video/build/source/Video.java
@@ -0,0 +1,65 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import processing.video.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Video extends PApplet {
+
+
+
+Capture cam;
+
+public void setup() {
+
+  //size(1280,960);
+  
+  
+  String[] cameras = Capture.list();
+
+  if (cameras.length == 0) {
+    println("There are no cameras available for capture.");
+    exit();
+  } else {
+    println("Available cameras:");
+    for (int i = 0; i < cameras.length; i++) {
+      println(cameras[i]);
+    }
+
+    // The camera can be initialized directly using an
+    // element from the array returned by list():
+    cam = new Capture(this, cameras[1]);
+    cam.start();
+
+  }
+}
+
+public void draw() {
+  if (cam.available() == true) {
+    cam.read();
+  }
+  image(cam, 0, 0);
+  // The following does the same, and is faster when just drawing the image
+  // without any additional resizing, transformations, or tint.
+  //set(0, 0, cam);
+}
+  public void settings() {  size(640, 480); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Video" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
