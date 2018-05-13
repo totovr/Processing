2c1ae326ae8a2631f31980d26d9e120c3ff5f694
Antonio Vega <a.vegaramirez76@gmail.com>
2018-05-13 17:50:37 +0900

    Update
    Render updated

--- KinectV2/Point_cloud_two/build-tmp/source/Point_cloud_two.java
+++ KinectV2/Point_cloud_two/build-tmp/source/Point_cloud_two.java
@@ -1,5 +1,23 @@
-//import processing.opengl.*; 
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
 import SimpleOpenNI.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Point_cloud_two extends PApplet {
+
+//import processing.opengl.*; 
+ 
 
 SimpleOpenNI kinect; 
 
@@ -11,13 +29,13 @@ PVector boxCenter = new PVector(0, 0, 600);
 // this will be used for zooming 
 float s = 1; 
 
-void setup() { 
-  size(1024, 768, P3D); 
+public void setup() { 
+   
   kinect = new SimpleOpenNI(this); 
   kinect.enableDepth(); 
 } 
 
-void draw() { 
+public void draw() { 
   background(0); 
   kinect.update(); 
   translate(width/2, height/2, -1000); 
@@ -40,16 +58,26 @@ void draw() {
 // up-arrow zooms in 
 // down arrow zooms out 
 // s gets passed to scale() in draw() 
-void keyPressed(){ 
+public void keyPressed(){ 
   if(keyCode == 38){ 
-    s = s + 0.01; 
+    s = s + 0.01f; 
   } 
 
   if(keyCode == 40){ 
-   s = s - 0.01; 
+   s = s - 0.01f; 
   } 
 } 
 
-void mousePressed(){ 
+public void mousePressed(){ 
   save("touchedPoint.png"); 
 } 
+  public void settings() {  size(1024, 768, P3D); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "--present", "--window-color=#666666", "--stop-color=#cccccc", "Point_cloud_two" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
